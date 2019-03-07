e03a1b73b8029622b1bdccec2e3a3e7d191bb407
CKillen <cl_killen@hotmail.com>
2019-03-07 00:30:35 -0500

    Updates

---
+++ precache-manifest.1727766ba360fc6d6cc836763b12eea8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "976821e7c7c83f0311bc",
+    "url": "/clickygame/static/js/runtime~main.976821e7.js"
+  },
+  {
+    "revision": "214b70e9208d661d5905",
+    "url": "/clickygame/static/js/main.214b70e9.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/clickygame/static/js/2.ac164eeb.chunk.js"
+  },
+  {
+    "revision": "82e5527436994e1dfc5da45add7ce26c",
+    "url": "/clickygame/index.html"
+  }
+];
